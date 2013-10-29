dd96abd36f94a430848f7383e9099e5af47dc53c
PeterDijkgraaf <peter@basegrow.com>
2013-10-29 21:28:10 +0100

    I did stuff

--- js/new.js
+++ js/new.js
@@ -96,14 +96,15 @@ function createHyperlapse(start_lat, start_lng, end_lat, end_lng) {
 				elevation: 50,
 				max_points: 75,
 				distance_between_points: 5,
-				millis: 125
+				millis: 125,
+				width: $(".big_wrapper").width(),
+				height: $(".big_wrapper").height()
 			});
 
 			hyperlapse.onRouteProgress = function(e) {
 				var p = Math.floor((hyperlapse.length()/160)*100);
 				$('.progress .bar').width(p+'%');
 				$('#bar_percent').html(p+'%');
-				$("#bar_percent").css("margin-left", p+ 5 + "%");
 			};
 
 			hyperlapse.onRouteComplete = function(e) {
@@ -114,14 +115,12 @@ function createHyperlapse(start_lat, start_lng, end_lat, end_lng) {
 				var p = Math.floor((((e.position+1)/hyperlapse.length()) *100 ) / 2) + 50;
 				$('.progress .bar').width(p+'%');
 				$('#bar_percent').html(p+'%');
-				$("#bar_percent").css("margin-left", p+ 5 +"%");
 				
 			};
 
 			hyperlapse.onLoadComplete = function(e) {
 				$('.progress .bar').width('0%');
 				$('#bar_percent').html('');
-				$("#bar_percent").css("margin-left", "0%");
 				hyperlapse.play();
 			};
 
